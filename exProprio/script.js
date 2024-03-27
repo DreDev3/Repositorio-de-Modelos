c82c7d0a8d6eb6bc8d8d2cc60b86083fd2823dae
DreDev3 <andreedson02@gmail.com>
2024-03-27 14:20:54 -0300

    refactor. alter contato.html, script.js, stylesc.css

--- exProprio/script.js
+++ exProprio/script.js
@@ -7,5 +7,29 @@ function relogio() {
 
     rel.innerHTML = `${h}:${m}`
 }
-setInterval(relogio, 1000)
+setInterval(relogio, 1000);
 //[FIM] Relógio
+
+//Envio do form de email
+(function() {
+    emailjs.init("N3QtUB-PvgOmGHV3f")
+  })()
+  
+  function enviar() {
+    var nome = document.getElementById("nome").value
+    var email = document.getElementById("email").value
+    var mensagem = document.getElementById("txtarea").value
+
+    var templateParams = {
+      from_name: nome,
+      from_email: email,
+      message: mensagem
+    };
+    
+    emailjs.send("service_3vd8n7q", "template_v1lpksb", templateParams)
+      .then(function(response) {
+        alert("E-mail enviado com sucesso!")
+      }, function(error) {
+        console.error("Erro ao enviar e-mail:", error)
+      })
+  }
