05569d63f0548a0ac4dc1be72aed5d88a6629edc
DreDev3 <andreedson02@gmail.com>
2024-04-22 13:41:59 -0300

    change index.html, script.js and style.css

---
+++ modelo-instagram/script.js
@@ -0,0 +1,56 @@
+//Botões de avançar e retroceder story
+function checkScroll() {
+    var container = document.getElementById('scrollContainer');
+    var scrollLeft = container.scrollLeft;
+    var scrollRight = container.scrollWidth - container.clientWidth - scrollLeft;
+
+    var scrollLeftButton = document.getElementById('scrollLeftButton');
+    var scrollRightButton = document.getElementById('scrollRightButton');
+
+    if (scrollLeft === 0) {
+        scrollLeftButton.style.display = 'none';
+    } else {
+        scrollLeftButton.style.display = 'block';
+    }
+
+    if (scrollRight === 0) {
+        scrollRightButton.style.display = 'none';
+    } else {
+        scrollRightButton.style.display = 'block';
+    }
+}
+
+function scrollLeft() {
+    var container = document.getElementById('scrollContainer');
+    container.scrollLeft -= 660;
+    checkScroll();
+}
+
+function scrollRight() {
+    var container = document.getElementById('scrollContainer');
+    container.scrollLeft += 660;
+    checkScroll();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var container = document.getElementById('scrollContainer');
+    container.addEventListener('scroll', checkScroll);
+    checkScroll();
+})
+//[FIM] Botões de avançar e retroceder story
+
+function options() {
+    // let botao = document.querySelector('.cancel')
+    let option = document.querySelector('.windowcenter')
+        option.style.display = 'block'
+
+        // botao.addEventListener(option.classList.toggle('active'))
+
+}
+
+function cancelar() {
+        option.style.zIndex = ''
+}
+
+
+
